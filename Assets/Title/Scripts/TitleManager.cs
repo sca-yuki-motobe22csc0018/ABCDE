085f5f5670368a52d3257fbfa7626e8e3faf5e8e
yukito187 <sdt22csc0019@edu.sca.ac.jp>
2025-09-27 18:49:35 +0900

    タイトル動き仮制作

--- Assets/Title/Scripts/TitleManager.cs
+++ Assets/Title/Scripts/TitleManager.cs
@@ -1,28 +1,81 @@
+using DG.Tweening;
+using NUnit.Framework.Internal;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using DG.Tweening;
 using UnityEngine.UI;
 
 public class TitleManager : MonoBehaviour
 {
-    [SerializeField] Image Card;
-    private int centerX = 960;
-    private int centerY = 540;
+    [SerializeField] Text clickStart;//clickstartText
+    [SerializeField] Text title;//titleText
+    public Sprite[] titleImages; // 画像を複数登録
+    public Image[] targetImages;    // 表示させるImageコンポーネント
+    //private int centerX = 960;//canvas用中心座標X
+    //private int centerY = 540;//canvas用中心座標Y
+
+    private bool clickCheck = false;//左クリックによるシーン移動誤爆阻止用
+    private float startAlpha = 1.0f;//クリックスタートの始まる時のアルファ値
+    private float alpha = 0.2f;//ClickStart用
+    private float alphaTime = 1.0f;//ClickStart用　動きにかかる時間
+    private int loop = -1;//ClickStart用　ループ
+
+    private float titleAlpha = 1.0f;//タイトルのアルファ値
+    private float titleTime = 1.0f;//fadeにかかる時間
+
+    private float titleImageAlpha = 1.0f;//タイトルimageのアルファ値
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        TitleMove();
+        if (titleImages.Length > 0 && targetImages != null)
+        {
+            for (int i = 0; i < titleImages.Length; i++) 
+            {
+                int index = Random.Range(0, titleImages.Length);
+                targetImages[i].sprite = titleImages[index];
+            }
+        }
+        Fade();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            gameEnd();
+        }
+
+        if (clickCheck)
+        {
+            if (Input.GetKeyDown(KeyCode.Mouse0))
+            {
+                changeScene();
+            }
+        }
     }
 
     void TitleMove()
     {
-        Card.transform.DOMove(new Vector3(centerX, centerY, 0),1).SetEase(Ease.Linear);
+        clickCheck = true;
+        Sequence seq = DOTween.Sequence();
+        seq.Append(clickStart.DOFade(alpha, alphaTime).SetEase(Ease.InSine));
+        seq.SetLoops(loop, LoopType.Yoyo);
+        //Sequence title = DOTween.Sequence();
+        //seq.Append(targetImages[0].transform.DOMoveY(-1080, 2.0f).SetEase(Ease.Linear));
+        //seq.Join(targetImages[1].transform.DOMoveY(-1080, 2.0f).SetEase(Ease.Linear));
+        //seq.Join(targetImages[2].transform.DOMoveY(-1080, 2.0f).SetEase(Ease.Linear));
+        //seq.SetLoops(loop, LoopType.Restart);
+    }
+
+    void Fade()
+    {
+        Sequence fade = DOTween.Sequence();
+        fade.Append(title.DOFade(titleAlpha, titleTime).SetEase(Ease.InSine));
+        fade.Join(clickStart.DOFade(startAlpha, titleTime).SetEase(Ease.InSine));
+        fade.Join(targetImages[0].DOFade(titleImageAlpha, titleTime).SetEase(Ease.InSine));
+        fade.OnComplete(() => TitleMove());
+
     }
 
     public void changeScene()
